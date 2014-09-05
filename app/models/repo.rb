bd64025297f9b1a4152f34a1f10760c90847db0a
fergmastaflex <joey@fergmedia.com>
2014-09-05 00:47:54 -0600

    Extracts some logic to the repo model

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,11 @@
 class Repo < ActiveRecord::Base
   belongs_to :user
+
+  def total_this_week(user)
+    github = Github.new oauth_token: user.token
+    closed_ones = github.issues.list(state: 'closed', repo: self.name, labels: '*', since: "#{Time.now.beginning_of_week(:monday).strftime('%F')}T00:00:00Z").count
+    closed_twos = github.issues.list(state: 'closed', repo: self.name, labels: '**', since: "#{Time.now.beginning_of_week(:monday).strftime('%F')}T00:00:00Z").count
+    closed_threes = github.issues.list(state: 'closed', repo: self.name, labels: '***', since: "#{Time.now.beginning_of_week(:monday).strftime('%F')}T00:00:00Z").count
+    total_done_this_week = (closed_ones + (closed_twos * 2) + (closed_threes * 3)).to_d
+  end
 end
