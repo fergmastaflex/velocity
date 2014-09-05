0a0d337df1cee244511bd5b154aa1d3dbd4d9c36
fergmastaflex <joey@fergmedia.com>
2014-09-04 23:25:15 -0600

    Adds a cool progress bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
     @github = Github.new oauth_token: current_user.token
+    ones = @github.issues.list(state: 'open', labels: '*').count
+    twos = @github.issues.list(state: 'open', labels: '**').count
+    threes = @github.issues.list(state: 'open', labels: '***').count
+    closed_ones = @github.issues.list(state: 'closed', labels: '*', since: "#{Time.now.beginning_of_week.strftime('%F')}T00:00:00Z").count
+    closed_twos = @github.issues.list(state: 'closed', labels: '**', since: "#{Time.now.beginning_of_week.strftime('%F')}T00:00:00Z").count
+    closed_threes = @github.issues.list(state: 'closed', labels: '***', since: "#{Time.now.beginning_of_week.strftime('%F')}T00:00:00Z").count
+    @total_pending = (ones + twos + threes).to_d
+    @total_done_this_week = (closed_ones + closed_twos + closed_threes).to_d
+    #weekly goal to be a project specific thing? until then, i'll do 15
+    @percent_complete = (@total_done_this_week.to_d / 15.to_d) * 100
   end
 end
